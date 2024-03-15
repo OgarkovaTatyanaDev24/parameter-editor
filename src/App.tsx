c1372e52ac03aa5b3203018eaaf1d2ae98141fbb
Tanya <taniachumakova@mail.ru>
2024-03-15 13:28:34 +0800

    create editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent } from 'react';
+import styles from "./app.module.css";
+
+interface Param {
+  id: number;
+  name: string;
+  type: string;
+}
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+interface NamedParamValue {
+  id: number;
+  value: string;
+  name: string;
+}
+interface Color {
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+interface Props {
+  params: Param[];
+  model: Model;
+}
+interface State {
+  model: Model
+  editableParams: NamedParamValue[]
+}
+
+class ParamEditor extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    const avalibleTypes = new Set(['string'])
+    const paramMap = props.model.paramValues.reduce((map, param) => map.set(param.paramId, param.value), new Map());
+    const editableParams: NamedParamValue[] = props.params.reduce((params: NamedParamValue[], param: Param) => {
+      if (avalibleTypes.has(param.type)) params.push({
+        id: param.id,
+        name: param.name,
+        value: paramMap.get(param.id) ? paramMap.get(param.id) : ''
+      })
+      return params
+    }, []);
+    this.state = { editableParams: editableParams, model: props.model };
+  }
+  public getModel(): Model {
+    return this.state.model
+  }
+  setStringParam(paramId: number, paramValue: string) {
+    this.setState({
+      model: {
+        paramValues: this.state.model.paramValues.map(param => {
+          return { paramId: param.paramId, value: param.paramId === paramId ? paramValue : param.value }
+        }),
+        colors: this.state.model.colors,
+      },
+      editableParams: this.state.editableParams.map(
+        param => param.id === paramId ? { id: paramId, value: paramValue, name: param.name } : param
+      ),
+    })
+  }
+  render(): React.ReactNode {
+    return (
+      <div className={styles.container}>
+        {this.state.editableParams.map(element => {
+          const handleChange = (e: ChangeEvent<HTMLInputElement>) => this.setStringParam(element.id, e.target.value)
+          return (
+            <div key={element.id + element.name}>
+              <p>{element.name}</p>
+              <input id={element.id + element.name} onInput={handleChange} value={element.value} />
+            </div>
+          )
+        })}
+        <text>{JSON.stringify(this.state.model)}</text>
+      </div>
+    )
+  }
+}
+
 
 function App() {
+  // Пример структуры:
+  const params: Param[] = [
+    {
+      "id": 1,
+      "name": "Назначение",
+      "type": "string"
+    },
+    {
+      "id": 2,
+      "name": "Длина",
+      "type": "string"
+    },
+    {
+      "id": 3,
+      "name": "Цена",
+      "type": "тгьиук"
+    }
+  ];
+  const model: Model = {
+    "paramValues": [
+      {
+        "paramId": 1,
+        "value": "повседневное"
+      },
+      {
+        "paramId": 2,
+        "value": "макси"
+      },
+      {
+        "paramId": 7,
+        "value": "otherVal"
+      }
+    ],
+    "colors": []
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParamEditor params={params} model={model} />
   );
 }
 
